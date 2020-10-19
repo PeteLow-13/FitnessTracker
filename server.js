b5e98604b284fa722e9a94cca633dc9ae555af67
Pete Low <pedritolow@gmail.com>
2020-10-19 14:05:00 -0700

    added server routes, router + routes , +  schema

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3000;
 
-const app =express();
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,12 +11,20 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongod://localhost/budget',{
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workouts',{
     useNewUrlParser: true,
     useFindAndModify: false
 });
 //routes
-app.use(require('./'))
+app.use(require('./routes/api.js'));
+app.get('/exercise', function(req,res){
+    res.sendFile(__dirname + '/public/exercise.html');
+});
+app.get('/stats', function(req,res){
+    res.sendFile(__dirname + '/public/stats.html');
+});
+
+
 //listener
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`);
