51e03b58ee0ffdfab8ae7f937a9405b4ba81e231
Pete Low <pedritolow@gmail.com>
2020-10-19 17:15:20 -0700

    updated db path

--- server.js
+++ server.js
@@ -11,10 +11,14 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+//database mongoose connection
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workouts',{
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify: false
 });
+
 //routes
 app.use(require('./routes/api.js'));
 app.get('/', function(req, res){
